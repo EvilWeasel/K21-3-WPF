43cb64a4e745557ad2b0f25f4095ff1543d6687e
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-07-04 15:31:57 +0200

    started mvvm

---
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            FirstName = "Dave";
+            MiddleName = "Hackerman";
+            LastName = "Jones";
+        }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
